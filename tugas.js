61e767d16831865c4bbf86ace24e8ba741f02dac
Aryosatyo Bhanurahman <bhanurahman919@gmail.com>
2023-03-02 14:56:11 +0700

    inisiasi project

---
+++ tugas.js
@@ -0,0 +1,222 @@
+console.log('1.Buatlah 20 variable yang dipakai untuk menghitung 5 angka')
+
+let angka1 = 2 + 2 * 2 - 3 + 10
+console.log('angka1 =', angka1)
+let angka2 = 3 + 7 * 10 * 15 - 28
+console.log('angka2 =', angka2)
+let angka3 = 10 / 10 * 8 / 2 + 20
+console.log('angka3 =', angka3)
+let angka4 = 4 + 20 * 96 - 100 + 10
+console.log('angka4 =', angka4)
+let angka5 = 95 - 90 * 1 - 5 + 35
+console.log('angka5 =', angka5)
+let angka6 = 2 ** 2 / 2 ** 3 % 2
+console.log('angka6 =', angka6)
+let angka7 = 28 + 10 * 15 - 4 - 50 
+console.log('angka7 =', angka7)
+let angka8 = 36 - 10 - 10 ** 2 % 3 
+console.log('angka8 =', angka8)
+let angka9 = 40 + 10 * 10 + 20 / 2 
+console.log('angka9 =', angka9)
+let angka10 = 28 + 10 * 15 - 4 - 50 
+console.log('angka10 =', angka10)
+let angka11 = 35 / 5 * 10 * 15 - 4 
+console.log('angka11 =', angka11)
+let angka12 = 100 / 100 * 53 * 50 * 0 
+console.log('angka12 =', angka12)
+let angka13 = 50 / 10 * 15 - 10 + 35 
+console.log('angka13 =', angka13)
+let angka14 = 100 + 50 * 15 ** 2 % 3 
+console.log('angka14 =', angka14)
+let angka15 = 35 * 100 * 1 - 32 - 53 
+console.log('angka15 =', angka15)
+let angka16 = 45 / 5 * 100 - 5 * 0 
+console.log('angka16 =', angka16)
+let angka17 = 100 - 50 * 35 / 10 % 4 
+console.log('angka17 =', angka17)
+let angka18 = 19 ** 1 * 15 / 4 % 2 
+console.log('angka18 =', angka18)
+let angka19 = 30 + 100 * 2 - 67 - 13 
+console.log('angka19 =', angka19)
+let angka20 = 100 / 10 ** 2 + 35 - 20
+console.log('angka20 =', angka20)
+
+
+console.log('2. buat masing masing 10 dari setiap operator perbandingan')
+
+console.log('Operator >')
+let perbandingan1 = angka1 > angka2
+console.log('angka1 > angka2', perbandingan1)
+let perbandingan2 = angka7 > angka3
+console.log('angka7 > angka3', perbandingan2)
+let perbandingan3 = angka10 > angka9
+console.log('angka10 > angka9', perbandingan3)
+let perbandingan4 = angka4 > angka5
+console.log('angka4 > angka5', perbandingan4)
+let perbandingan5 = angka12 > angka6
+console.log('angka12 > angka6', perbandingan5)
+let perbandingan6 = angka9 > angka7
+console.log('angka9 > angka7', perbandingan6)
+let perbandingan7 = angka20 > angka8
+console.log('angka20 > angka8', perbandingan7)
+let perbandingan8 = angka8 > angka9
+console.log('angka8 > angka9', perbandingan8)
+let perbandingan9 = angka10 > angka13
+console.log('angka10 > angka13', perbandingan9)
+let perbandingan10 = angka19 > angka1
+console.log('angka19 > angka1', perbandingan10)
+
+console.log('Operator <')
+let perbandingan11 = angka1 < angka2
+console.log('angka1 < angka2', perbandingan1)
+let perbandingan12 = angka5 < angka9
+console.log('angka5 < angka9', perbandingan12)
+let perbandingan13 = angka8 < angka20
+console.log('angka8 < angka20', perbandingan13)
+let perbandingan14 = angka16 < angka17
+console.log('angka16 < angka17', perbandingan14)
+let perbandingan15 = angka11 < angka7
+console.log('angka11 < angka7', perbandingan15)
+let perbandingan16 = angka2 < angka7
+console.log('angka2 < angka7', perbandingan16)
+let perbandingan17 = angka8 < angka9
+console.log('angka8 < angka9', perbandingan17)
+let perbandingan18 = angka15 < angka20
+console.log('angka15 < angka20', perbandingan18)
+let perbandingan19 = angka1 < angka3
+console.log('angka1 < angka3', perbandingan19)
+let perbandingan20 = angka10 < angka20
+console.log('angka10 < angka20', perbandingan20)
+
+console.log('Operator >=')
+let perbandingan21 = angka5 >= angka3
+console.log('angka5 >= angka3', perbandingan21)
+let perbandingan22 = angka1 >= angka11
+console.log('angka1 >= angka11', perbandingan22)
+console.log('Operator >=')
+let perbandingan23 = angka8 >= angka1
+console.log('angka8 >= angka1', perbandingan23)
+let perbandingan24 = angka15 >= angka7
+console.log('angka15 >= angka7', perbandingan24)
+let perbandingan25 = angka9 >= angka9
+console.log('angka9 >= angka9', perbandingan25)
+let perbandingan26 = angka15 >= angka20
+console.log('angka15 >= angka20', perbandingan26)
+let perbandingan27 = angka15 >= angka1
+console.log('angka15 >= angka1', perbandingan27)
+let perbandingan28 = angka1 >= angka1
+console.log('angka1 >= angka1', perbandingan28)
+let perbandingan29 = angka9 >= angka2
+console.log('angka9 >= angka2', perbandingan29)
+let perbandingan30 = angka7 >= angka9
+console.log('angka7 >= angka9', perbandingan30)
+
+console.log('Operator <=')
+let perbandingan41 = angka3 <= angka2
+console.log('angka3 <= angka2', perbandingan41)
+let perbandingan42 = angka9 <= angka14
+console.log('angka9 <= angka14', perbandingan42)
+let perbandingan43 = angka1 <= angka10
+console.log('angka1 <= angka10', perbandingan43)
+let perbandingan44 = angka13 <= angka11
+console.log('angka13 <= angka11', perbandingan44)
+let perbandingan45 = angka8 <= angka13
+console.log('angka8 <= angka13', perbandingan45)
+let perbandingan46 = angka19 <= angka20
+console.log('angka19 <= angka20', perbandingan46)
+let perandingan47 = angka11 <= angka11
+console.log('angka11 <= angka11', perandingan47)
+let perbandingan48 = angka10 <= angka3
+console.log('angka10 <= angka3', perbandingan48)
+let perbandingan49 = angka11 <= angka12
+console.log('angka11 <= angka12', perbandingan49)
+let perbandingan50 = angka6 <= angka7
+console.log('angka6 <= angka7', perbandingan50)
+
+console.log('Operator ==')
+let perbandingan51 = angka1 == angka4
+console.log('angka1 == angka4', perbandingan51)
+let perbandingan52 = angka12 == angka12
+console.log('angka12 == angka12', perbandingan52)
+let perbandingan53 = angka13 == angka20
+console.log('angka13 == angka20', perbandingan53)
+let perbandingan54 = angka13 == angka19
+console.log('angka13 == angka19', perbandingan54)
+let perbandingan55 = angka1 == angka1
+console.log('angka1 == angka1', perbandingan55)
+let perbandingan56 = angka7 == angka9
+console.log('angka7 == angka9', perbandingan56)
+let perbandingan57 = angka19 == angka9
+console.log('angka19 == angka9', perbandingan57)
+let perbandingan58 = angka13 == angka2
+console.log('angka13 == angka2', perbandingan58)
+let perbandingan59 = angka12 == angka17
+console.log('angka12 == angka17', perbandingan59)
+let perbandingan60 = angka12 == angka8
+console.log('angka12 == angka8', perbandingan60)
+
+console.log('Operator ===')
+let perbandingan61 = angka2 === angka2
+console.log('angka2 === angka2', perbandingan61)
+let perbandingan62 = angka8 === angka1
+console.log('angka8 === angka1', perbandingan62)
+let perbandingan63 = angka9 === angka17
+console.log('angka9 === angka17', perbandingan63)
+let perbandingan64 = angka11 === angka15
+console.log('angka11 === angka15', perbandingan64)
+let perbandingan65 = angka10 === angka18
+console.log('angka10 === angka18', perbandingan65)
+let perbandingan66 = angka13 === angka4
+console.log('angka13 === angka4', perbandingan66)
+let perbandingan67 = angka17 === angka15
+console.log('angka17 === angka15', perbandingan67)
+let perbandingan68 = angka11 === angka12
+console.log('angka11 === angka12', perbandingan68)
+let perbandingan69 = angka10 === angka12
+console.log('angka10 === angka12', perbandingan69)
+let perbandingan70 = angka15 === angka13
+console.log('angka15 === angka13', perbandingan70)
+
+console.log('Operator !=')
+let perbandingan71 = angka1 != angka1
+console.log('angka1 != angka1', perbandingan71)
+let perbandingan72 = angka12 != angka2
+console.log('angka12 != angka2', perbandingan72)
+let perbandingan73 = angka9 != angka5
+console.log('angka9 != angka5', perbandingan73)
+let perbandingan74 = angka8 != angka9
+console.log('angka8 != angka9', perbandingan74)
+let perbandingan75 = angka14 != angka20
+console.log('angka14 != angka20', perbandingan75)
+let perbandingan76 = angka20 != angka20
+console.log('angka20 != angka20', perbandingan76)
+let perbandingan77 = angka16 != angka1
+console.log('angka16 != angka1', perbandingan77)
+let perbandingan78 = angka16 != angka4
+console.log('angka16 != angka4', perbandingan78)
+let perbandingan79 = angka5 != angka20
+console.log('angka5 != angka20', perbandingan79)
+let perbandingan80 = angka19 != angka9
+console.log('angka13 != angka6', perbandingan80)
+
+console.log('Operator !==')
+let perbandingan81 = angka2 !== angka3
+console.log('angka2 !== angka3', perbandingan81)
+let perbandingan82 = angka5 !== angka17
+console.log('angka5 !== angka17', perbandingan82)
+let perbandingan83 = angka14 !== angka17
+console.log('angka14 !== angka17', perbandingan83)
+let perbandingan84 = angka9 !== angka19
+console.log('angka9 !== angka19', perbandingan84)
+let perbandingan85 = angka14 !== angka6
+console.log('angka14 !== angka6', perbandingan85)
+let perbandingan86 = angka20 !== angka19
+console.log('angka20 !== angka19', perbandingan86)
+let perbandingan87 = angka15 !== angka16
+console.log('angka15 !== angka16', perbandingan87)
+let perbandingan88 = angka1 !== angka1
+console.log('angka1 !== angka1', perbandingan88)
+let perbandingan89 = angka20 !== angka19
+console.log('angka20 !== angka19', perbandingan89)
+let perbandingan90 = angka4 !== angka1
+console.log('angka4 !== angka1', perbandingan90)
